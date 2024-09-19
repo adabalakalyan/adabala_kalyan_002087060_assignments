c57c1ad009d4cbc664c9b5b48ee3cdcb6b25c5e4
Kalyan Adabala <adabala.k@northeastern.edu>
2024-09-20 04:31:38 +0530

    Merge origin/main into main
    Conflicts:
    	README.md

---
+++ PersonalProfile/src/model/Product.java
@@ -0,0 +1,194 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author nanik
+ */
+public class Product {
+    
+    String firstname;
+    String lastname;
+    String email;
+    String phone;
+    String driverlicensenumber;
+    String driverlicensestate;
+    String ssn;
+    String addressline1;
+    String addressline2;
+    String city;
+    String state;
+    String zip;
+    String telephone;
+    String fax;
+    String nationality;
+    String relationshipstatus;
+    String occupation;
+    String incomeperannum;
+    String Height;
+    String Weight;
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getDriverlicensenumber() {
+        return driverlicensenumber;
+    }
+
+    public void setDriverlicensenumber(String driverlicensenumber) {
+        this.driverlicensenumber = driverlicensenumber;
+    }
+
+    public String getDriverlicensestate() {
+        return driverlicensestate;
+    }
+
+    public void setDriverlicensestate(String driverlicensestate) {
+        this.driverlicensestate = driverlicensestate;
+    }
+
+    public String getSsn() {
+        return ssn;
+    }
+
+    public void setSsn(String ssn) {
+        this.ssn = ssn;
+    }
+
+    public String getAddressline1() {
+        return addressline1;
+    }
+
+    public void setAddressline1(String addressline1) {
+        this.addressline1 = addressline1;
+    }
+
+    public String getAddressline2() {
+        return addressline2;
+    }
+
+    public void setAddressline2(String addressline2) {
+        this.addressline2 = addressline2;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getZip() {
+        return zip;
+    }
+
+    public void setZip(String zip) {
+        this.zip = zip;
+    }
+
+    public String getTelephone() {
+        return telephone;
+    }
+
+    public void setTelephone(String telephone) {
+        this.telephone = telephone;
+    }
+
+    public String getFax() {
+        return fax;
+    }
+
+    public void setFax(String fax) {
+        this.fax = fax;
+    }
+
+    public String getNationality() {
+        return nationality;
+    }
+
+    public void setNationality(String nationality) {
+        this.nationality = nationality;
+    }
+
+    public String getRelationshipstatus() {
+        return relationshipstatus;
+    }
+
+    public void setRelationshipstatus(String relationshipstatus) {
+        this.relationshipstatus = relationshipstatus;
+    }
+
+    public String getOccupation() {
+        return occupation;
+    }
+
+    public void setOccupation(String occupation) {
+        this.occupation = occupation;
+    }
+
+    public String getIncomeperannum() {
+        return incomeperannum;
+    }
+
+    public void setIncomeperannum(String incomeperannum) {
+        this.incomeperannum = incomeperannum;
+    }
+
+    public String getHeight() {
+        return Height;
+    }
+
+    public void setHeight(String Height) {
+        this.Height = Height;
+    }
+
+    public String getWeight() {
+        return Weight;
+    }
+
+    public void setWeight(String Weight) {
+        this.Weight = Weight;
+    }
+    
+}
